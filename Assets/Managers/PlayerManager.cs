7e30890b1ba2dd525ab8efbb2568edecd4fb9108
scso-ucsc <scso@ucsc.edu>
2024-04-24 16:57:16 -0700

    Adding UI and SoarStatus

--- Assets/Managers/PlayerManager.cs
+++ Assets/Managers/PlayerManager.cs
@@ -6,8 +6,9 @@ public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager instance;
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float radioForce = 10f; //Speed of flight
+    [SerializeField] private float radioForce = 5.0f; //Speed of flight (Min: 5.0f, Max 10.0f)
     [SerializeField] private LayerMask groundLayer;
+    private bool soarStatus;
 
     void Awake(){
         if(instance == null){
@@ -20,7 +21,7 @@ public class PlayerManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        soarStatus = false;
     }
 
     // Update is called once per frame
@@ -28,10 +29,15 @@ public class PlayerManager : MonoBehaviour
     {
         if(Input.GetButton("Jump")){ //While the SPACE button is down, player will float up
             rb.velocity = Vector2.up * radioForce;
+            soarStatus = true;
         }
-
         if(Input.GetButtonUp("Jump")){
             rb.velocity = Vector2.up * 0f;
+            soarStatus = false;
         }
+    }
+
+    public bool returnSoarStatus(){ //For future AudioManager
+        return soarStatus;
     }
 }
