0d36ebb527c7f45cbc4b3e5dc45f6c39663d3be8
scso-ucsc <scso@ucsc.edu>
2024-04-24 18:14:23 -0700

    Updating PlayerMovement

--- Assets/Managers/PlayerManager.cs
+++ Assets/Managers/PlayerManager.cs
@@ -6,9 +6,8 @@ public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager instance;
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float radioForce = 5.0f; //Speed of flight (Min: 5.0f, Max 10.0f)
+    [SerializeField] private float radioVolume = 5.0f; //Speed of flight (Min: 5.0f, Max 25.0f)
     [SerializeField] private LayerMask groundLayer;
-    private bool soarStatus;
 
     void Awake(){
         if(instance == null){
@@ -21,23 +20,24 @@ public class PlayerManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        soarStatus = false;
+        radioVolume = 5.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButton("Jump")){ //While the SPACE button is down, player will float up
-            rb.velocity = Vector2.up * radioForce;
-            soarStatus = true;
-        }
-        if(Input.GetButtonUp("Jump")){
+        if(radioVolume > 0){ //Add upwards force to player based on radio volume
+            rb.velocity = Vector2.up * radioVolume;
+        } else{
             rb.velocity = Vector2.up * 0f;
-            soarStatus = false;
         }
     }
 
-    public bool returnSoarStatus(){ //For future AudioManager
-        return soarStatus;
+    public void onSliderChanged(float value){ //Update Radio Volume upon slider change
+        radioVolume = value;
+    }
+
+    public float getRadioForce(){ //For UI
+        return radioVolume;
     }
 }
