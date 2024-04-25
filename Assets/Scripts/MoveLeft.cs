28a591f8407b947a6f3f6375674b3c4948f3ee1f
akhalim1 <156042742+akhalim1@users.noreply.github.com>
2024-04-25 11:50:45 -0700

    uploading character sprites, background (WIP), and cloud sprite

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 1f;
 
     // Update is called once per frame
     void Update()
