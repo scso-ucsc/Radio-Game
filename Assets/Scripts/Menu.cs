e6d0f108ebc254ee53563bdfd6a919cce3879f5a
hrong1 <97559375+hrong1@users.noreply.github.com>
2024-04-27 15:50:46 -0700

    Pause menu(C#)

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    public static bool GameIsPause = false;
+
+    public GameObject Paused;
+
     public void GamePause()
     {
         Time.timeScale = 0;
+        Paused.SetActive(true);
+        GameIsPause = true;
     }
 
     public void GameStart()
     {
         Time.timeScale = 1;
+        Paused.SetActive(false);
+        GameIsPause = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPause == true){
+                GameStart();
+            }else{
+                GamePause();
+            }
+        }    
     }
 }
