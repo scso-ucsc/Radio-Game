4a41418d71ae45c5b5a5b0b940b1e1cd412d6f80
scso-ucsc <scso@ucsc.edu>
2024-04-28 12:48:55 -0700

    Adding Dynamic UI

--- Assets/Managers/UIManager.cs
+++ Assets/Managers/UIManager.cs
@@ -7,6 +7,7 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private GameObject onButton, offButton, pauseButton, playButton;
 
     void Awake(){
         if(instance == null){
@@ -19,12 +20,31 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        onButton.SetActive(true); //Setting UI Buttons
+        offButton.SetActive(false);
+        pauseButton.SetActive(true);
+        playButton.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = GameManager.instance.getPlayerScore().ToString();
+        
+        if(PlayerManager.instance.getPlayerHasAmmo() == true){ //Updating on-off button when player has ammo
+            onButton.SetActive(true);
+            offButton.SetActive(false);
+        } else{ //PlayerManager.instance.getPlayerHasAmmo() == false
+            onButton.SetActive(false);
+            offButton.SetActive(true);
+        }
+
+        if(PlayerManager.instance.getIsFiringStatus() == true){
+            pauseButton.SetActive(false);
+            playButton.SetActive(true);
+        } else{ //PlayerManager.instance.getIsFiringStatus() == false
+            pauseButton.SetActive(true);
+            playButton.SetActive(false);
+        }
     }
 }
